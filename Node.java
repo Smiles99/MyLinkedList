1f700a2ba5da501314541601bd48924a94474a9d
Smiles99 <mdavies10@stuy.edu>
2021-01-12 11:49:23 -0500

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,8 +1,14 @@
 public class Node{
  public Node(String value){
-   
+
  }
  private String data;
  private Node next,prev;
- //write get/set methods for all three instance variables.
+ public static String get(int index) {
+   if (index < 0 || index >= data.size()) {
+     throw new IllegalArgumentException("Invalid Index");
+   } else {
+
+   }
+ }
 }
